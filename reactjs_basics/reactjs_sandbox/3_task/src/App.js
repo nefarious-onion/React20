8d202939ac719eab94ebc66055d6320cbb9365c3
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-03-30 11:12:50 +0300

    add buttons and event handlers

--- reactjs_basics/reactjs_sandbox/3_task/src/App.js
+++ reactjs_basics/reactjs_sandbox/3_task/src/App.js
@@ -1,14 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   state = {
-    likes : 0
+    likes: 0
+  }
+
+  addHandler = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  }
+  removeHandler = () => {
+    this.setState({
+      likes: this.state.likes -1
+    });
+  }
+  resetHandler = () => {
+    this.setState({
+      likes: 0
+    })
   }
   render() {
     return (
       <div>
         <h1>Total likes: {this.state.likes}</h1>
+        {/* function is inside the class so we need this keyword */}
+        <button onClick={this.addHandler} >Add like</button>
+        <button onClick={this.removeHandler} >Remove like</button>
+        <button onClick={this.resetHandler} >Reset likes</button>
       </div>
     );
   }
