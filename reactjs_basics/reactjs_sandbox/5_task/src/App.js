d683c18273d5275629051dcdf0c80b83d83af250
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-04-06 17:12:19 +0300

    add new state current, add random number generator to change the state current

--- reactjs_basics/reactjs_sandbox/5_task/src/App.js
+++ reactjs_basics/reactjs_sandbox/5_task/src/App.js
@@ -6,7 +6,25 @@ class App extends Component {
 
   state = {
     score: 0,
-    id: ''
+    id: '',
+    current: 0
+  }
+
+  getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  determineNext = () => {
+    let nextActive;
+    do {
+      nextActive = this.getRandomNumber(1, 4);
+    } while (this.state.current === nextActive);
+  
+
+    this.setState({
+      current: nextActive
+    }, () => {
+      console.log('current is', this.state.current);
+    })
   }
 
   clickHandler = e => {
@@ -20,18 +38,19 @@ class App extends Component {
   }
 
   render() {
+    
     return (
       <div className='container--main' >
         <h1 className='header--main' >The Speed Game</h1>
-    <div className='score-box' >Score: {this.state.score}</div>
+        <div className='score-box' >Score: {this.state.score}</div>
         <div className='circle-box'>
-          <Circle id='1' clickHandler={this.clickHandler}/>
-          <Circle id='2' clickHandler={this.clickHandler}/>
-          <Circle id='3' clickHandler={this.clickHandler}/>
-          <Circle id='4' clickHandler={this.clickHandler}/>
+          <Circle id='1' clickHandler={this.clickHandler} />
+          <Circle id='2' clickHandler={this.clickHandler} />
+          <Circle id='3' clickHandler={this.clickHandler} />
+          <Circle id='4' clickHandler={this.clickHandler} />
         </div>
         <div className='btn-box'>
-          <button className='btn'>Start Game</button>
+          <button onClick={this.determineNext} className='btn'>Start Game</button>
           <button className='btn'>End Game</button>
         </div>
 
@@ -41,9 +60,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-// const getRndInteger = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) ) + min;
-// };
