6cbbeb0936817914e6953e16d0aa7bcb32f6e3c9
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-04-29 10:17:09 +0300

    add components

---
+++ reactjs_basics/reactjs_sandbox/8_task/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SimpleRouting from './SimpleRouting';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <SimpleRouting />
+    </div>
+  );
+}
+
+export default App;
--- reactjs_basics/reactjs_sandbox/8_task/src/index.js
+++ reactjs_basics/reactjs_sandbox/8_task/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SimpleRouting from './lesson/SimpleRouting';
+ 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
