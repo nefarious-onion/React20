4980082e0d56156f9ec02a686c73c25586b16871
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-04-29 11:53:02 +0300

    add Post component

---
+++ reactjs_basics/reactjs_sandbox/8_task/src/components/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Post.css';
+
+const Post = () => {
+    return (
+        <div className='post'>
+            <h3>Title</h3>
+            <p>Author</p>
+            <p>post-text</p>
+        </div>
+    );
+}
+
+export default Post;
