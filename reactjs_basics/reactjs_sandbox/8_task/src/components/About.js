efac5965f777620addc29f7767d8dc0383d9d9ff
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-03 14:37:26 +0300

    Minor fixes to about page

--- reactjs_basics/reactjs_sandbox/8_task/src/components/About.js
+++ reactjs_basics/reactjs_sandbox/8_task/src/components/About.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './About.css';
+import aboutImage from '../images/aboutImage.jpg';
 
 const About = () => {
     return (
         <div className='about-container'>
-            <img className='about-img' src='../../public/images/aboutImage.jpg' alt='flower in the field' width="100%"></img>
+            <img className='about-img' src={aboutImage} alt='flower in the field' width="300px" height="100%"/>
             <div className='about-text'>
+                <h1>About me</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Aperiam numquam facilis unde, velit magni assumenda minima
                 quis molestiae officiis ullam reiciendis magnam, excepturi
                 corporis nemo repudiandae eveniet praesentium quibusdam sint.
                 </p>        
                 <p>
-                orem ipsum dolor sit amet consectetur adipisicing elit.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Aperiam numquam facilis unde, velit magni assumenda minima
                 quis molestiae officiis ullam reiciendis magnam, excepturi
                 corporis nemo repudiandae eveniet praesentium quibusdam sint.
