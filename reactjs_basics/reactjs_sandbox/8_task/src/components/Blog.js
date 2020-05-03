7e512b971adccd6b26551ed6fa3911badfe803f7
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-03 14:40:00 +0300

    Add map to render list of posts, add some styling

--- reactjs_basics/reactjs_sandbox/8_task/src/components/Blog.js
+++ reactjs_basics/reactjs_sandbox/8_task/src/components/Blog.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Post from './Post';
 import './Blog.css';
+import postData from '../data/postData.json';
 
-const Blog = () => {
+//const posts = postData;
+class Blog extends React.Component {
+    state = {
+        posts: [...postData]
+    }
+
+    //use switch between postlist and single post (use parameters)
+    render() {
+        console.log(this.state.posts)
+
+        const posts = [...this.state.posts]
+            .map( ({id, title, image, author, content}) => 
+            <Post 
+                key={id}
+                title={title}
+                image={image}
+                author={author}
+                content={content. substring(0, 50) + '...'}
+            />);
     return (
-        <div>
-            <Post/>
+        <div className='blog-container'>
+            {posts}
         </div>
     );
+    }
 }
 
 export default Blog;
