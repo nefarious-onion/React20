7e87b1ae83078434bf7ac997500e74099778f8af
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-03 14:38:28 +0300

    Add props to Post, add styling

--- reactjs_basics/reactjs_sandbox/8_task/src/components/Post.js
+++ reactjs_basics/reactjs_sandbox/8_task/src/components/Post.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Post.css';
 
-const Post = () => {
+const Post = ({title, author, image, content}) => {
+    console.log(image);
     return (
-        <div className='post'>
-            <h3>Title</h3>
-            <p>Author</p>
-            <p>post-text</p>
+        <div className='post-item'>
+            <img src={image} height='80px'/>
+            <h3>{title}</h3>
+            <p>{author}</p>
+            <p>{content}</p>
+            <button>Read More</button>
         </div>
     );
 }
