458a7dd4272b4d376e32cfd49697e73ad2d26f88
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-06 11:02:55 +0300

    update postlist

--- reactjs_basics/reactjs_sandbox/8_task/src/components/Postlist/Postlist.js
+++ reactjs_basics/reactjs_sandbox/8_task/src/components/Postlist/Postlist.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import Post from '../Post/Post';
+import { Link, useParams } from "react-router-dom";
 import './Postlist.css'
 
-const Postlist = ({posts}) => {
+
+const Postlist = ({ posts }) => {
 
     const postList = posts
         .map(({ id, title, image, author, content }) =>
-            <Post
-                key={id}
-                id={id}
-                title={title}
-                image={image}
-                author={author}
-                content={content.substring(0, 50) + '...'}
-            />);
+            <div className='postlist-item'>
+                <img src={image} height='80px' />
+                <h3>{title}</h3>
+                <p>{author}</p>
+                <p>{content.substring(0, 50) + '...'}</p>
+                <Link to={`/blog/${id}`} >Read More</Link>
+            </div>)
+
     return (
         <div className='postlist-container'>
             {postList}
